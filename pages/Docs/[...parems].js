4309e7779cf76d7e0dfec5f3fb59cd1dd08e269c
Azeezjim <azeexjim@gmail.com>
2022-03-26 05:03:52 -1100

    1st commit

--- pages/Docs/[...parems].js
+++ pages/Docs/[...parems].js
@@ -1,12 +1,18 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 function Doc() {
   const router = useRouter();
-  const {parems } =router.query
-  console.log("parems", parems)
-  return (
-    <div>Doc Home Page</div>
-  )
+  const { parems = [] } = router.query;
+  console.log("parems", parems);
+  if (parems.length === 2) {
+    return (
+      <h1>
+        Viewing docs for {parems[0]} and {parems[1]}
+      </h1>
+    )
+  } else if (parems.length === 1) {
+    return <h1> Viewing docs for {parems[0]}</h1>;
+  }  return <h1>Doc Home Page</h1>;
 }
 
-export default Doc
+export default Doc;
