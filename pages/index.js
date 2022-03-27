af29b6c9d9b008108189032ac1b697476d00a3e5
Azeezjim <azeexjim@gmail.com>
2022-03-26 19:27:48 -1100

    1st  commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import {useRouter} from 'next/router '
 
 function Home() {
+  const handleClick = () => { 
+    console.log('lace your order');
+  }
   return(
     <div>
       <h1>Home Page </h1>
@@ -11,6 +15,9 @@ function Home() {
       <Link href='./Products' > 
         <a >Products</a>
       </Link>
+      <button onClick={handleClick}>
+        Place Holder
+      </button>
     </div>
   )
 }
