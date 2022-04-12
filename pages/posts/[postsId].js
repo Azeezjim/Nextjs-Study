450aa3628b6b325b09e26a8977264f4fd19252ff
Azeezjim <azeexjim@gmail.com>
2022-04-12 05:53:04 -1100

    latest commit of the day

--- pages/posts/[postsId].js
+++ pages/posts/[postsId].js
@@ -1,25 +1,64 @@
+import { useRouter } from "next/router";
 
 function Posts({ post }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      <h2>{post.id} {post.title}</h2>
-      <hr/>
+      <h2>
+        {post.id} {post.title}
+      </h2>
+      <hr />
       {post.body}
     </>
-    )
+  );
 }
 
-export default Posts
+export default Posts;
 
+export async function getStaticPaths() {
+  // const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const data = await response.json();
+
+  // const paths = data.map((post) => {
+  //   return {
+  //     params: {
+  //       postsId: `${post.id}`,
+  //     },
+  //   };
+  // });
+
+
+  return {
+    // paths,
+    paths: [
+      { params: { postsId: '1' } },
+      { params: { postsId: '2' } },
+      { params: { postsId: '3' } }
+    ],
+    fallback: true,
+  }
+}
 
 export async function getStaticProps(context) {
-  const {params } = context;
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const { params } = context;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postsId}`
+  );
   const data = await response.json();
+    if(!data.id) {
+      return {
+        notFound: true
+      }
+    }
+
+  console.log(`generating gage for /posts/${params.postsId}`);
 
   return {
     props: {
-      post: data.slice(0, 10),
+      post: data,
     },
   };
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -8,14 +8,12 @@ function PostLists({ posts }) {
         return (
           <div key={post.id}>
             <Link href={`posts/${post.id}`} passHref>
-              <a>
                 <h2>
                   {post.id} {post.title}
                 </h2>
-              </a>
             </Link>
             <hr />
-            {/* <p>{post.name}</p> */}
+            {/* <p>{post.name}</p> */} 
             {/* <p>{post.email}</p> */}
           </div>
         );
@@ -26,29 +24,6 @@ function PostLists({ posts }) {
 
 export default PostLists;
 
-export async function getStaticPaths() {
-  return {
-    paths: [
-      {
-        params: { postId: "1" },
-      },
-      {
-        params: { postId: "2" },
-      },
-      {
-        params: { postId: "3" },
-      },
-      {
-        params: { postId: "4" },
-      },
-      {
-        params: { postId: "5" },
-      },
-    ],
-    fallback: false,
-
-  };
-}
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -56,7 +31,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: data.slice(0, 5),
-    },
+      posts: data.slice(0, 3),
+    }, 
   };
 }
