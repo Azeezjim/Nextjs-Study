35ecd5c72a171f15250381a7ed3ea1bf5206b3e2
Azeezjim <azeexjim@gmail.com>
2022-06-19 05:12:30 -1100

    latest commit of the day

--- pages/dashboard-swr.js
+++ pages/dashboard-swr.js
@@ -1,7 +1,24 @@
 import useSWR  from "swr";
+const featcher = async () => {
+  const response = await fetch('http://localhost:4000/dashboard')
+  const data = await response.json()
+  return data;
+}
 
 function DatabaseSWR() {
-  useSWR('dashboard', async () => {
-    const response = await fetch()
-  })
+  const { data, error } = useSWR('dashboard', featcher )
+
+  if( error) return 'An error occored'
+  if (!data) return 'Loading...'
+
+
+  return (
+    <div>
+      <h2>SWR Dashboard</h2>
+      <h2>Posts - {data.posts}</h2>
+      <h2>Likes - {data.likes}</h2>
+      <h2>Followers - {data.followers}</h2>
+      <h2>Following - {data.following}</h2>
+    </div>
+  )
 }
